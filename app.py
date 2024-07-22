39171b8e761dd23cd10d3e3d8bccd2217562f95b
Jonghun Kim <corner.jhkim@gmail.com>
2024-07-22 19:32:04 +0900

    Added Speech

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import tkinter as tt
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
+from gtts import gTTS
+from playsound import playsound
+import time as t
 
 from functions import *
 
-# 2024 jul 22st mon pm 08:25 utc+09:00
+# Lastest Edited | 2024 jul 22st mon pm 07:31 utc+09:00
 # X(Twitter) @benevolent2023 | Discord nerdmecha#1806 | GameJolt @nerdmecha
 
-figureCloud() # active words & clouds
+# figureCloud() # active words & clouds
+inputSpeaker() # active langs & speech
--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 import tkinter as tt
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
+from gtts import gTTS
+from playsound import playsound
+import time as t
 
 # words & clouds
 def figureCloud():
@@ -27,3 +30,24 @@ cloud = WordCloud(
     height=1000,
     colormap='rainbow'
 ).generate_from_frequencies(words)
+
+# langs & speech
+def inputSpeaker():
+    tts.save("auds/speech.mp3")
+    t.sleep(5)
+    playsound("auds/speech.mp3")
+
+
+lang = input("Select Your Language - English or Korean : ")
+
+while "English" not in lang and "Korean" not in lang:
+    lang = input("PLZ Input Again - English or Korean : ")
+
+if "English" in lang:
+    text = input("Input Eng Lines : ")
+    tts = gTTS(text=text, lang='en')
+elif "Korean" in lang:
+    text = input("한국어로 문장을 입력하세요 : ")
+    tts = gTTS(text=text, lang='ko')
+else:
+    tts = gTTS(text="Good Bye", lang='en')
